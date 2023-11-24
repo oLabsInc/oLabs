7d49383c88a4e35757ec28c8f21ef3932c7301dc
oLabs <jonny.olabs@gmail.com>
2023-11-24 12:16:53 -0500

    updated file system connected scripts and styles - added basic device logic

--- public/logic/main.js
+++ public/logic/main.js
@@ -1,5 +1,6 @@
 const
-    coreContainer = document.getElementById('coreContainer')
+    coreContainer = document.getElementById('coreContainer'),
+    coreContainerBackgroundImg = document.querySelector('#coreContainerBackground img')
 
 
 
@@ -25,6 +26,8 @@ function getDeviceDetails() {
     deviceDimensions.availW = window.screen.availWidth
     deviceDimensions.availH = window.screen.availHeight
     deviceDimensions.isExtended = window.screen.isExtended
+    deviceDimensions.currentOrientation = screen.orientation.type
+    deviceDimensions.currentOrientationAngle = screen.orientation.angle
     // RESPONSE FROM LAPTOP: Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36 Edg/119.0.0.0
 
 
@@ -58,7 +61,7 @@ function getDeviceDetails() {
 }
 
 function init() {
-
+    updateDeviceOrientation()
 
     // GET USER CAMERAS/MICROPHONES/SPEAKERS
     navigator.mediaDevices.enumerateDevices().then(function (devices) {
@@ -79,5 +82,21 @@ function updateDeviceOrientation(type, angle) {
     deviceDimensions.currentOrientationAngle = angle
     // console.log(deviceDimensions)
 
+    console.log(coreContainer)
+    if (deviceDimensions.currentOrientation === 'landscape-primary') {
+        coreContainer.innerText = JSON.parse(deviceDimensions.h)
+        coreContainer.innerText = 'Device is ' + JSON.parse(deviceDimensions.h) + 'px wide in Landscape\nTurn device upright for 2D\nDevice is ready for VR and AR'
+        coreContainer.classList.remove('device-orientation-portrait')
+        coreContainer.classList.add('device-orientation-landscape')
+        coreContainerBackgroundImg.style.maxWidth = '50%'
+        coreContainerBackgroundImg.style.maxHeight = '50%'
+        
+    } else {
+        coreContainer.innerText = 'Device is ' + JSON.parse(deviceDimensions.w) + 'px wide in Portrait\nCurrently experiencing oLabs in 2D\nTurn device sideways to enter VR or AR'
+        coreContainer.classList.remove('device-orientation-landscape')
+        coreContainer.classList.add('device-orientation-portrait')
+        coreContainerBackgroundImg.style.maxWidth = '80%'
+        coreContainerBackgroundImg.style.maxHeight = '80%'
+    }
 
 }
